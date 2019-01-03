057ebd10b523480ecee4b541f1924ccecd8e442d
tonaramos <gtonaramos@gmail.com>
2019-01-03 11:32:27 -0800

    Commit test.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,8 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+//test for update
+
 function anagrams(stringA, stringB) {}
 
 module.exports = anagrams;
