34fab5329c5820c891637fb62930fdc882624ef5
tonaramos <gtonaramos@gmail.com>
2019-01-03 10:50:15 -0800

    Added solution for reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let output = '';
+  for (let i=str.length-1; i>=0; i--) {
+    output += str[i];
+  }
+  return output;
+}
 
 module.exports = reverse;
