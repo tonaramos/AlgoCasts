1747bb1fed55c48fa695789d04768daa3ecf89ce
tonaramos <gtonaramos@gmail.com>
2019-01-08 11:47:21 -0800

    Final Solution.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,39 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+
+
+//  -- Third Solution  --           
+// function palindrome(str) {                      // this solution does double the work since only the first half of items need to be compared
+//   return str.split('').every((char, i) => {     // no need to compare the last half of the characters
+//     return char === str[str.length - 1 - i];
+//   })
+// }
+
+
+// -- Second Solution --
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//   return str === reversed;
+// }
+
+
+//  -- First Solution  --
+function palindrome(str) {
+  let reverseIndex = str.length - 1;
+  let output = true;
+  str.split('').map(item => {
+    if (item !== str[reverseIndex]) {
+      output = false;
+    }
+    // console.log('item -> ', item, ' === ', str[reverseIndex], ' ???');
+    reverseIndex -= 1; 
+  })
+  return output;
+}
+// console.log(palindrome('abgh'));
+// console.log(palindrome('uytyu'));
+
 
 module.exports = palindrome;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,31 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//  --  Second solution --
+function reverseInt(n) {
+  const reverse = n.toString().split('').reverse().join('');
+  return parseInt(reverse) * Math.sign(n);
+}
+
+
+
+//  -- First solution --
+// function reverseInt(n) {
+//   const str = n.toString();
+  
+//   if (str[0] == '-') {
+//     let output = '';
+//     for (let i = str.length -1; i > 0; i--) {
+//       output += str[i];
+//     }
+//     output = '-' + output;
+//     return parseInt(output);
+//   }
+ 
+//   return parseInt(str.split('').reverse().join('')); 
+// }
+
+// console.log(reverseInt(-51));
+// console.log(reverseInt(987));
 
 module.exports = reverseInt;
