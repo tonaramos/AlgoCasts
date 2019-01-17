7061f40c7100e42ec38f8706522a8e37c1bd98e5
tonaramos <gtonaramos@gmail.com>
2019-01-16 17:04:31 -0800

    Final solution.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,8 +8,57 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-//second for update
+// -- Third Solution
 
-function anagrams(stringA, stringB) {}
+const anagrams = (stringA, stringB) => {
+  let str1 = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  let str2 = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  return str1 === str2;
+}
 
+// -- second solution 
+
+// function anagrams(stringA, stringB) {
+//   let str1 = stringA.replace(/[^\w]/g, '').toLowerCase().split('');
+//   let str2 = stringB.replace(/[^\w]/g, '').toLowerCase().split('');
+//   let obj1 = {};
+//   str1.map((item) => {
+//     obj1[item] = obj1[item] + 1 || 1;
+//   })
+//   str2.map((item) => {
+//     obj1[item] ? obj1[item]-- : obj1[item] = -1;
+//   })
+//   return Object.values(obj1).every(item => item === 0);
+// }
+
+
+
+// -- first solution --
+// function anagrams(stringA, stringB) {
+//   let str1 = stringA.replace(/[^\w]/g, '').toLowerCase().split('');
+//   let str2 = stringB.replace(/[^\w]/g, '').toLowerCase().split('');
+//   let obj1 = {};
+//   let obj2 = {};
+//   str1.map((item, index) => {
+//     if (obj1[item]) {
+//       obj1[item]++;
+//     } else {
+//       obj1[item] = 1;
+//     }
+//   })
+//   str2.map((item, index) => {
+//     if (obj2[item]) {
+//       obj2[item]++;
+//     } else {
+//       obj2[item] = 1;
+//     }
+//   })
+//   let output = true;
+//   Object.keys(obj2).map(key => {
+//     if (obj1[key] !== obj2[key]) {
+//       output = false;
+//     }
+//   })
+//   return output;
+// }
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//  -- Second Solution --
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+
+//  -- First Solution --
+// function chunk(array, size) {
+//   let count = 0;
+//   let output = [];
+//   let tempArr = [];
+//   for (let i=0; i<array.length; i++) {
+//     tempArr.push(array[i]);
+//     count++;
+//     if (count%size === 0 || i === array.length-1) {
+//       output.push(tempArr);
+//       tempArr = [];
+//     }
+//   }
+//   return output;
+// }
 
 module.exports = chunk;
